0a88bfb832e33720c20bc1bb63608ae84df4f46b
Jack <jzha471@aucklanduni.ac.nz>
2019-03-02 19:25:44 +1300

    Creating the Login repository method

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -14,9 +15,35 @@ namespace DatingApp.API.Data
         {
             _context = context;
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Username == username);
+            if( user == null)
+            {
+                return null;
+            }
+            if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for( int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
 
         public async Task<User> Register(User user, string password)
@@ -39,11 +66,15 @@ namespace DatingApp.API.Data
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
             
-        }
+        } 
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            if(await _context.Users.AllAsync( x => x.Username == username))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -8,7 +8,7 @@ namespace DatingApp.API.Models
     public class User
     {
         public int Id { get; set; }
-        public string username { get; set; }
+        public string Username { get; set; }
 
         public byte[] PasswordHash { get; set; }
 
