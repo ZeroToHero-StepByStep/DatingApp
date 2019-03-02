86ffe1fc0fed935497fdff3eeeca2c27efb47d44
Jack <jzha471@aucklanduni.ac.nz>
2019-03-02 22:02:32 +1300

    Creating the Register method in our Auth Controller

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController: ControllerBase 
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            //validate the request 
+            username = username.ToLower();
+            if(await _repo.UserExists(username))
+            {
+                return BadRequest("Username already exists");
+            }
+            var userToCreate = new User
+            {
+                Username = username
+            };
+
+            var createduser = await _repo.Register(userToCreate, password);
+            return StatusCode(201);
+        }
+
+    }
+}
