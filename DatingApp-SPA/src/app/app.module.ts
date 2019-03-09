13fc8ee5a2bafcbe12be26dfda09c7e92ff81f94
Jack <jzha471@aucklanduni.ac.nz>
2019-03-09 22:51:58 +1300

    Parent to Child Component communication using Input properties

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
@@ -12,9 +11,8 @@ import { RegisterComponent } from './register/register.component';
   declarations: [
     AppComponent
 ,
-    ValueComponent
+    NavComponent
 ,
-    NavComponent,
     HomeComponent,
     RegisterComponent
 ],
