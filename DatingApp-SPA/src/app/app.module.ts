48d5af3210d138210a11f142f1139e6a6f70d52e
Jack <jzha471@aucklanduni.ac.nz>
2019-03-13 22:48:16 +1300

    Handling errors in Angular

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { NavComponent } from './nav/nav.component';
 import {FormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { AuthService } from './_services/auth.service';
+import{ErrorInterceptorProvider} from './_services/error.interceptor';
 @NgModule({
   declarations: [
     AppComponent
@@ -18,10 +19,13 @@ import { RegisterComponent } from './register/register.component';
 ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    HttpClientModule, 
     FormsModule
   ],
-  providers: [],
+  providers: [
+      AuthService,
+      ErrorInterceptorProvider
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
