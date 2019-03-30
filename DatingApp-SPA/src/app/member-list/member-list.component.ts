f630a161742b5b363100dadcde727a1612ede034
Jack <jzha471@aucklanduni.ac.nz>
2019-03-30 22:50:55 +1300

    Retrieving the members into the member List component

--- DatingApp-SPA/src/app/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/member-list/member-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {User} from '../_models/user'
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
-})
+}) 
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  users:User[];
+  constructor(private userService:UserService, private alertify: AlertifyService) { }
+
+
+  loadUsers(){
+    this.userService.getUsers().subscribe((users:User[])=>{
+      this.users = users;
+    }, error =>{
+      this.alertify.error(error); 
+    })
+  }
 
   ngOnInit() {
+    this.loadUsers();
   }
 
 }
