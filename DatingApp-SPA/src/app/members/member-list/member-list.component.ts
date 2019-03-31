3b3e284fdda65e4c753299b365a73a2f0819c79b
Jack <jzha471@aucklanduni.ac.nz>
2019-03-31 20:16:16 +1300

    Creating members cards to display on our member list page

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../_models/user'
-import { UserService } from '../_services/user.service';
-import { AlertifyService } from '../_services/alertify.service';
+import {User} from '../../_models/user'
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
@@ -13,6 +13,10 @@ export class MemberListComponent implements OnInit {
   constructor(private userService:UserService, private alertify: AlertifyService) { }
 
 
+  ngOnInit() {
+    this.loadUsers();
+  }
+
   loadUsers(){
     this.userService.getUsers().subscribe((users:User[])=>{
       this.users = users;
@@ -21,8 +25,5 @@ export class MemberListComponent implements OnInit {
     })
   }
 
-  ngOnInit() {
-    this.loadUsers();
-  }
 
 }
