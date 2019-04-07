0737c8d5fc677d347767ad79f2e434d97edae729
Jack <jzha471@aucklanduni.ac.nz>
2019-04-07 22:29:13 +1200

    Adding a photo gallery to our application

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import {MemberDetailResolver} from './_resolver/member-detail.resolver';
 import { MemberListResolver } from './_resolver/member-list.resolver';
+import {NgxGalleryModule} from 'ngx-gallery';
 
 export function tokenGetter(){
     return localStorage.getItem('token');
@@ -53,6 +54,7 @@ export function tokenGetter(){
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
     JwtModule.forRoot({
         config:{
             tokenGetter: tokenGetter,
--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import {User} from '../../_models/user'
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
@@ -11,6 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 export class MemberDetailComponent implements OnInit {
 
   user:User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   constructor(private userService:UserService, private alertify:AlertifyService,
     private route:ActivatedRoute) {
 
@@ -19,7 +23,33 @@ export class MemberDetailComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data =>{
       this.user = data['user'];
-    })
+    });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+
+  }
+
+  getImages(){
+    const imageUrls = [];
+    for ( let i = 0; i < this.user.photos.length; i ++){
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imageUrls;
   }
 
   // members/4
