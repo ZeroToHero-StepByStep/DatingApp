b7ef5f8b1e55ebc197e84ac1a170ecb69c6529e4
Jack <jzha471@aucklanduni.ac.nz>
2019-05-05 20:09:47 +1200

    Using the array filter method to provide instant feedback in the SPA

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -18,6 +18,7 @@ export class PhotoEditorComponent implements OnInit {
   uploader:FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
+  currentMain:Photo;
 
   constructor(private authService: AuthService, private userService: UserService,private alertify:AlertifyService) { }
 
@@ -64,7 +65,9 @@ export class PhotoEditorComponent implements OnInit {
   setMainPhoto(photo: Photo){
     this.userService.setMainPhoto(this.authService.decodedToken.nameid,photo.id)
     .subscribe(()=>{
-      console.log('Successfully set to main');
+      this.currentMain = this.photos.filter(p => p.isMain == true)[0];
+      this.currentMain.isMain = false;
+      photo.isMain = true;
     }, error =>{
       this.alertify.error(error);
     });
