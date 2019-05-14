52c3c0e1dab289712c692fdd376135b52cf788b8
Jack <jzha471@aucklanduni.ac.nz>
2019-05-14 20:58:18 +1200

    Introduction to Reactive Forms in Angualr

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import{HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthService } from './_services/auth.service';
@@ -59,6 +59,7 @@ export function tokenGetter(){
     BrowserModule,
     HttpClientModule, 
     FormsModule,
+    ReactiveFormsModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
