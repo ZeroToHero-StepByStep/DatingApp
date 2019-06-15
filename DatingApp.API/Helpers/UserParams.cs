ab984d70a734342a53821bd1890c23bef307c97a
Jack <jzha471@aucklanduni.ac.nz>
2019-06-15 17:13:28 +1200

    Adding additional filtering parameters to the API

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -22,7 +22,9 @@ namespace DatingApp.API.Helpers
 
         public string Gender { get; set; }
 
+        public int MinAge { get; set; } = 18;
 
+        public int MaxAge { get; set; } = 99;
 
     }
 }
